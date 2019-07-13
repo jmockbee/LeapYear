946cf385a2e5f53da8356abccc9b7e234fa530d4
Jon Mockbee <mockbee1000@gmail.com>
2019-07-13 09:25:52 -0400

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        boolean itsaLeapYear = LeapYear.isLeapYear(2020);
+        System.out.println("Is it a Leap Year? = " + itsaLeapYear );
+    }
 }
